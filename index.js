4bde6fa0f09fc2a0ce9c68f42cd45b6d411ef0a0
SMoreno5 <98197583+SMoreno5@users.noreply.github.com>
2022-07-07 14:07:30 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+
+/// Dice img Randomizer ///
+var randomNumber1 = Math.floor((Math.random()*6))+1;
+var randomNumber2 = Math.floor((Math.random()*6))+1;
+document.getElementsByTagName("img")[0].setAttribute("src", "images/dice"+ randomNumber1 +".png");
+document.getElementsByTagName("img")[1].setAttribute("src", "images/dice"+ randomNumber2 +".png");
+
+
+/// Winner Announcement ///
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!"
+} else if (randomNumber1< randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩"
+} else {
+  document.querySelector("h1").innerHTML = "Draw! :("
+}
